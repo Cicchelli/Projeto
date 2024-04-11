7a1498d3f6f3033d4b8eabb618788a21316fe974
Stephan <cicchelli.stephan@gmail.com>
2024-04-11 16:49:19 -0300

    req 6

--- src/controllers/translate_controller.py
+++ src/controllers/translate_controller.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from models.language_model import LanguageModel
+from deep_translator import GoogleTranslator
 
 translate_controller = Blueprint("translate_controller", __name__)
 
@@ -11,5 +12,23 @@ def get_translate():
         "translate_to": "en",
         "translated": "What do you want to translate?"
     }
+    languages = LanguageModel.list_dicts()
+    return render_template("index.html", languages=languages, **default_translation)
+
+@translate_controller.route("/", methods=["POST"])
+def post_translate():
+    text_to_translate = request.form.get("text-to-translate")
+    translate_from = request.form.get("translate-from")
+    translate_to = request.form.get("translate-to")
+
+    translated = GoogleTranslator(source=translate_from, target=translate_to).translate(text_to_translate)
+
+    default_translation = {
+        "text_to_translate": text_to_translate,
+        "translate_from": translate_from,
+        "translate_to": translate_to,
+        "translated": translated
+    }
+
     languages = LanguageModel.list_dicts()
     return render_template("index.html", languages=languages, **default_translation)
