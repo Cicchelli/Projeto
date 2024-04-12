56367d6247cb8dd87886f270ebcd4739efdae561
Stephan <cicchelli.stephan@gmail.com>
2024-04-12 10:30:15 -0300

    req 9

--- src/controllers/translate_controller.py
+++ src/controllers/translate_controller.py
@@ -2,6 +2,8 @@ from flask import Blueprint, render_template, request
 from models.language_model import LanguageModel
 from deep_translator import GoogleTranslator
 
+from models.history_model import HistoryModel
+
 translate_controller = Blueprint("translate_controller", __name__)
 
 @translate_controller.route("/", methods=["GET"])
@@ -17,21 +19,29 @@ def get_translate():
 
 @translate_controller.route("/", methods=["POST"])
 def post_translate():
-    text_to_translate = request.form.get("text-to-translate")
-    translate_from = request.form.get("translate-from")
-    translate_to = request.form.get("translate-to")
+    text_to_translate = request.form["text-to-translate"]
+    translate_from = request.form["translate-from"]
+    translate_to = request.form["translate-to"]
 
     translated = GoogleTranslator(source=translate_from, target=translate_to).translate(text_to_translate)
 
-    default_translation = {
+    history_data = {
         "text_to_translate": text_to_translate,
         "translate_from": translate_from,
         "translate_to": translate_to,
-        "translated": translated
     }
+    HistoryModel(history_data).save()
 
-    languages = LanguageModel.list_dicts()
-    return render_template("index.html", languages=languages, **default_translation)
+    return render_template(
+        "index.html",
+        languages=LanguageModel.list_dicts(),
+        text_to_translate=text_to_translate,
+        translate_from=translate_from,
+        translate_to=translate_to,
+        translated=translated
+    )
+
+
 
 @translate_controller.route("/reverse", methods=["POST"])
 def reverse_translate():
--- tests/controllers/admin/test_admin_controller.py
+++ tests/controllers/admin/test_admin_controller.py
@@ -1,5 +1,5 @@
-# from src.models.history_model import HistoryModel
-# from src.models.user_model import UserModel
+# # from src.models.history_model import HistoryModel
+# # from src.models.user_model import UserModel
 
 
 def test_history_delete(app_test):
